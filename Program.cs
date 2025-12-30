bb784e0c17aa35209e08d50ae5fab26b1a06b4cb
Francesco <francesco-lion@virgilio.it>
2025-12-30 17:55:21 +0100

    chore: added services Google & DotEnv.Core

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using DotEnv.Core;
 
-app.MapGet("/", () => "Hello World!");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+new EnvLoader().Load();
+builder.Services.AddRazorPages();
+builder.Services.AddAuthentication(defaultScheme: "Cookie")
+    .AddCookie(authenticationScheme: "Cookie", (opt) =>
+    {
+      opt.Cookie.Name = "sso-auth";
+      opt.ExpireTimeSpan = TimeSpan.FromHours(8);
+      opt.LoginPath = "/login";
+    })
+    .AddGoogle("Google", (opt) =>
+    {
+      opt.ClientId = Environment.GetEnvironmentVariable("SSO_CLIENT_ID") ?? "";
+      opt.ClientSecret = Environment.GetEnvironmentVariable("SSO_CLIENT_SECRET") ?? "";
+      opt.CallbackPath = "/sso-return-url"; // Set this url to google ( defaault: /signin-google )
+      // opt.SignInScheme = "Cookie"; ( default: defaultScheme, "Cookie" )
+    });
 
+WebApplication app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapRazorPages()
+    .RequireAuthorization();
+app.MapGet("/", () => Results.Redirect("/login"));
 app.Run();
